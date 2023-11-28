11f9f1affecf16513a38a0c47d0e3a552c6da656
deelight-del <akingbenidavidoyin@gmail.com>
2023-11-28 15:10:26 +0000

    task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,28 +9,11 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t l_leaves, r_leaves;
 
-	if (binary_is_leaf(tree) == 1)
-		return (1);
-	else
-	{
-		l_leaves = binary_tree_leaves(tree->left);
-		r_leaves = binary_tree_leaves(tree->right);
-		return (l_leaves + r_leaves);
-	}
-}
-
-/**
- * binary_tree_is_leaf - Function to test if a given node of a tree is a leaf.
- * @node: The node to test.
- *
- * Return: 1 if node, otherwise 0.
- */
-int binary_is_leaf(const binary_tree_t *node)
-{
-	if (node == NULL)
-		return (-1);
-	if (node->left == NULL && node->right == NULL)
-		return (1);
-	else
+	if (tree == NULL)
 		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	l_leaves = binary_tree_leaves(tree->left);
+	r_leaves = binary_tree_leaves(tree->right);
+	return (l_leaves + r_leaves);
 }
