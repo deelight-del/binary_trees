a0cea473254e7db55e411590ed8b6af9c5e75f91
deelight-del <akingbenidavidoyin@gmail.com>
2023-11-28 12:17:00 +0000

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height, right_height;
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 	if (right_height >= left_height)
