60f795ce5275b814f89ceea90cf567bab2d88b82
deelight-del <akingbenidavidoyin@gmail.com>
2023-11-30 09:59:16 +0000

    fix part issues

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,5 +1,25 @@
 #include "binary_trees.h"
-#include <stdio.h>
+#include <limits.h>
+/**
+ * is_bst_utils - Function to help determine of a given tree is bst or not.
+ * @tree: The node to the root tree.
+ * @min: The minimum value in a bound to compare our node value with.
+ * @max: The maximum value in a bound to compare our node values with.
+ *
+ * Return: 0 or 1.
+ */
+int is_bst_utils(binary_tree_t *tree, int min, int max)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (1);
+	if (tree->n <= min || tree->n >= max)
+		return (0);
+	left = is_bst_utils(tree->left, min, tree->n);
+	right = is_bst_utils(tree->right, tree->n, max);
+	return (left * right);
+}
 /**
  * binary_tree_is_bst - Check if a tree is a binary tree.
  * @tree: The pointer to the root node.
@@ -8,24 +28,13 @@
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	int left_bst, right_bst, final_bst;
+	int left_bst, right_bst;
+
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
+	if (tree->right == NULL && tree->left == NULL)
 		return (1);
-	if (tree->left && (tree->left->n < tree->n)
-			&& tree->right && (tree->n < tree->right->n))
-	{
-		left_bst = binary_tree_is_bst(tree->left);
-		right_bst = binary_tree_is_bst(tree->right);
-		final_bst = (left_bst * right_bst);
-	}
-	else
-	{
-		left_bst = binary_tree_is_bst(tree->left);
-		right_bst = binary_tree_is_bst(tree->right);
-		final_bst = (left_bst * right_bst);
-	}
-	return (final_bst);
-
+	left_bst = is_bst_utils(tree->left, INT_MIN, INT_MAX);
+	right_bst = is_bst_utils(tree->right, INT_MIN, INT_MAX);
+	return (left_bst * right_bst);
 }
